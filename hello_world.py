02de6c0bb64a04b03fd9380366371f1e6cef9a01
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-10-19 04:24:42 +0100

    Initial scene

--- hello_world.py
+++ hello_world.py
@@ -1,19 +1,29 @@
-import sys
-import platform
-
-print("hello")
-
-print(1, sys.version)
-print(2, platform.python_implementation())
-print(3, sys.executable)
-
+# import sys
+# import platform
+#
+# print("hello")
+#
+# print(1, sys.version)
+# print(2, platform.python_implementation())
+# print(3, sys.executable)
+#
 
 from direct.showbase.ShowBase import ShowBase
 
+
 class MyApp(ShowBase):
 
-        def __init__(self):
-            ShowBase.__init__(self)
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
 
 app = MyApp()
 app.run()
