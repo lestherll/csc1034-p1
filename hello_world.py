b5ff58c472a261ac7e736e4881b68675b7337b07
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-10-19 04:19:18 +0100

    Added app class

--- hello_world.py
+++ hello_world.py
@@ -6,3 +6,14 @@ print("hello")
 print(1, sys.version)
 print(2, platform.python_implementation())
 print(3, sys.executable)
+
+
+from direct.showbase.ShowBase import ShowBase
+
+class MyApp(ShowBase):
+
+        def __init__(self):
+            ShowBase.__init__(self)
+
+app = MyApp()
+app.run()
