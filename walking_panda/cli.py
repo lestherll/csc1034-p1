5189b305d7f604f2739ad13eceb292109299f55e
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-10-19 05:36:34 +0100

    Added parser for cli.py

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -1,6 +1,14 @@
 from . import panda
 
+import argparse
+
+
 def cli():
-    walking = panda.WalkingPanda()
+    parser = argparse.ArgumentParser(prog="walking_panda")
+    parser.add_argument("--no-rotate", help="Suppress Rotation",
+                        action="store_true")
+    args = parser.parse_args()
+
+    walking = panda.WalkingPanda(**vars(args))
     walking.run()
 
